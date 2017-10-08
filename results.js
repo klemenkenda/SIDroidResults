e1545bfbb0d0901d5d344b617401f3561f82c79c
Klemen Kenda <klemen.kenda@ijs.si>
2017-10-08 14:52:13 +0200

    Removed cookie handling functions as we are using localStorage.

--- results.js
+++ results.js
@@ -2,33 +2,6 @@
 // AUTHOR: Klemen Kenda (2017)
 // DESCRIPTION: Part of OK Azimut SI-Droid results web application.
 
-// cookie handling functions
-function createCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name,"",-1);
-}
-
-
 /**
  * Results
  * Class, dealing with IOF XML v3 result file.
